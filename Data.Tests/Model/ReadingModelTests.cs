270b2fa0a3633b94d71314dcff568a4c2b894e9b
ncoxage <nev@coxage.me.uk>
2021-07-28 19:58:47 +0100

    Added ReadingModel

---
+++ Data.Tests/Model/ReadingModelTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+using Xunit;
+
+using Data;
+using Data.Model;
+
+
+namespace Data.Tests
+{
+    public class ReadingModelTests
+    {
+        [Fact]
+        public void ctor_PropertiesInitialiedCorrectly()
+        {
+            int id = 123;
+            DateTime readAt = DateTime.UtcNow;
+            int value = 987654;
+
+            var sut = new ReadingModel(accountId: id,
+                                       time: readAt,
+                                       value: value);
+
+            sut.AccountId.Should().Be(id);
+            sut.ReadAt.Should().Be(readAt);
+            sut.Value.Should().Be(value);
+        }
+    }
+}
---
+++ Data/Model/ReadingModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Model
+{
+    public class ReadingModel
+    {
+        public int AccountId { get; private set; }
+        public DateTime ReadAt { get; private set; }
+        public int Value { get; private set; }
+
+        public ReadingModel( int accountId, DateTime time, int value)
+        {
+            AccountId = accountId;
+            ReadAt = time;
+            Value = value;
+        }
+    }
+}
